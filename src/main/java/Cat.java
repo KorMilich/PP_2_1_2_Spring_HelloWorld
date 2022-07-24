cc9267a0d3a84669b35c81326aebc78f3d0d2b65
Mikle <korostelev@97.ingos.ru>
2022-07-24 16:35:38 +0300

    Java pre-project. Практическая задача 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Component;
+
+
+public class Cat {
+    public Cat() {
+        System.out.println("Cat been is created");
+    }
+
+    public void sayMeow(){
+        System.out.println("meow-meow");
+    }
+
+}
